a6d8641d6e8afb6cfd9a4a9a11c88bde6b1e0692
Boris Laporte <boris.laporte@gmail.com>
2016-06-16 00:59:03 +0200

    update Punchline

--- app/src/main/java/borislaporte/lipstyapp/model/ThePunchlines.java
+++ app/src/main/java/borislaporte/lipstyapp/model/ThePunchlines.java
@@ -11,12 +11,12 @@ public class ThePunchlines {
 
         this.punchlines[0] = new Punchline("yellow", "I've made a bright yellow drink to thank you for illuminating my day.");
         this.punchlines[1] = new Punchline("red", "This cocktail is red. Red is the color of fire and there is a fire that lighted up the moment I met you.");
-        this.punchlines[2] = new Punchline("brown", "I've made a bright yellow drink to thank you for illuminating my day");
-        this.punchlines[3] = new Punchline("pink", "I've made a bright yellow drink to thank you for illuminating my day");
+        this.punchlines[2] = new Punchline("brown", "has anyone told you're magnificent ? Will you let me offer this drink to you ? After all you stole my heart.");
+        this.punchlines[3] = new Punchline("pink", "Pink and crazy is this cocktail, as my heart falling for you.");
         this.punchlines[4] = new Punchline("blue", "I turned as blue as this drink when I met you.");
-        this.punchlines[5] = new Punchline("green", "I've always thought aliens where green and then I met you.\n(looks confused)\nWell you seem to beautiful to be human so you must be an alien right ? Do you want a green cocktail ?");
-        this.punchlines[6] = new Punchline("clear", "I'll make you a cocktail as dark as my world before you entered it");
-        this.punchlines[7] = new Punchline("null", "I've made a bright yellow drink to thank you for illuminating my day");
+        this.punchlines[5] = new Punchline("green", "I've always thought aliens where green and then I met you.\n(looks confused)\nWell you seem too beautiful to be human so you must be an alien right ? Do you want a green cocktail ?");
+        this.punchlines[6] = new Punchline("clear", "I've made you a cocktail as clear as you shrine before me");
+        this.punchlines[7] = new Punchline("null", "Please accept this gift, I've made it juste for you.");
     }
 
     public String getByColor(String color){
